3c8b8b00982aa493c13f58e0137e9c6936f16d02
Julian Horatschek <julian.horatschek@gmail.com>
2025-06-23 23:41:17 +0200

    Parser mostly done

--- src/parser/command_parser.cpp
+++ src/parser/command_parser.cpp
@@ -1,6 +1,28 @@
-#include <command_parser.hpp>
+#include "command_parser.hpp"
 
 namespace rewrite {
 
+    bool is_whitespace(const char c) {
+	return std::isspace(c) || c == ';' || c == '?' || c == '*';
+    }
 
+    bool is_number(const char c) {
+	return std::isdigit(c) || c == '+' || c == '-' || c == ',' || c == '.' || c == 'e' || c == 'E';
+    }
+
+    bool is_identifier(const char c) {
+	return std::isalpha(c) || c == '_';
+    }
+
+    bool is_quote(const char c) {
+	return c == '"';
+    }
+
+    bool is_string(const char c) {
+	return c != '"';
+    }
+
+    bool is_equals(const char c) {
+	return c == '=';
+    }
 }
--- src/parser/command_parser.hpp
+++ src/parser/command_parser.hpp
@@ -3,40 +3,121 @@
 #include <cctype>
 #include <expected>
 #include <vector>
+#include <charconv>
+#include <sstream>
 
 #include "polaris_commands.hpp"
 
 namespace rewrite {
 
+    /**
+     *
+     */
+    template<typename... Args>
+    std::string comp_error(const std::string& msg, Args... args) {
+	return (std::ostringstream(msg) << ... << args).str();
+    }
+
+
+    /**
+     *
+     */
     using CharCheckFn = bool(*)(const char);
 
-    bool is_whitespace(const char c) { return std::isspace(c); }
-    bool is_number(const char c) { return std::isdigit(c) || c == '+' || c == '-' || c == ',' || c == '.' || c == 'e' || c == 'E'; }
-    bool is_identifier(const char c) { return std::isalpha(c) || c == '_'; }
-    bool is_quote(const char c) { return c == '"'; }
+    bool is_whitespace(const char c);
+    bool is_number(const char c);
+    bool is_identifier(const char c);
+    bool is_quote(const char c);
+    bool is_string(const char c);
+    bool is_equals(const char c);
 
+
+    /**
+     *
+     */
     struct ParsedParameter {
 	enum class Type {
 	    Identifier,
 	    String,
 	    Number
-	}					type;
-	std::string_view			value;
+	};
 
-	double as_number() {
+	std::string_view	value;
+	Type			type;
 
+	/**
+	 *
+	 */
+	auto as_number() -> std::expected<double, std::string> {
+	    double	result{0};
+	    auto 	[ptr, ec] = std::from_chars(value.begin(), value.end(), result);
+
+	    if (ec == std::errc{})
+		return result;
+	    if (ec == std::errc::invalid_argument)
+		return std::unexpected { "Could not convert number" };
+	    return std::unexpected{ "Unknown conversion Error" };
 	}
 
-    }
-
-    struct ParsedLine {
-	CommandFileOption			command;
-	unsigned char				names_parameter_count;
-	std::vector<std::string_view>		parameters;
-
-
+	/**
+	 *
+	 */
+	auto unquoted() -> std::string_view {
+	    return value.substr(1, value.length() - 2);
+	}
     };
 
+
+    /**
+     * Fully parsed line with all found parameters
+     * The decision to use the parameters-vector to store named parameters
+     * as prepended name-value pairs was made as maps won't have much of
+     * a performance increase with small numbers of parameters, but will
+     * take much more space.
+     *
+     * @ivar command: Holds the command (<command>) of this line
+     * @ivar paramters: List of all found parameters. Named parameters are
+     * 			prepended as pairs (2 indices per named parameter as
+     * 			name - value pair). The effective index of positional
+     * 			parameters starts at named_parameter_count * 2 + 0
+     * 	@ivar named_parameter_count: Number of named parameters
+     *
+     */
+    struct ParsedLine {
+	std::string_view		command;
+	std::vector<ParsedParameter>	parameters;
+	unsigned int			named_parameter_count{0};
+
+	/**
+	 * Get named parameter by its name
+	 * @param name: String representing the desired parameter
+	 * @returns: An unquoted string of the found named parameter or
+	 * 	     std::unexpected if no matching parameter was found.
+	 */
+	auto get_named(const std::string& name)
+	    -> std::expected<std::string_view, std::string> {
+	    for(auto i = 0; i < named_parameter_count; i += 2)
+		if (parameters[i].value == name)
+		    return parameters[i + 1].unquoted();
+	    return std::unexpected { comp_error( "Unknown named parameter ", name) };
+	}
+
+	/**
+	 *
+	 */
+	auto get_value(const unsigned int i) const {
+	    return parameters[named_parameter_count * 2 + i];
+	}
+
+	auto operator[](const unsigned int i) const {
+	    return get_value(i);
+	}
+    };
+
+
+    /**
+     *
+     */
     class CommandParser {
 	private:
 	    struct Token {
@@ -52,52 +133,109 @@ namespace rewrite {
 	    std::string_view::const_iterator	pos;
 	    unsigned int			line_nr{0};
 	
+	    /**
+	     *
+	     */
 	    template<CharCheckFn check>
 	    std::string_view read_while() {
 		const auto	start = pos;
 
-		while (++pos != current_line.end() && check(*pos));
+		while (++pos < current_line.cend() && check(*pos));
 
 		return current_line.substr(
-		    std::distance(current_line.begin(), start),
+		    std::distance(current_line.cbegin(), start),
 		    std::distance(start, pos));
 	    }
 
+	    /**
+	     *
+	     */
+	    template<CharCheckFn check>
+	    bool expect_next() {
+		read_while<is_whitespace>();
+		return pos < current_line.cend() && check(*pos);
+	    }
+
+	    /**
+	     *
+	     */
+	    auto get_command(ParsedLine& result)
+		-> std::expected<bool, std::string> {
+
+		if (!expect_next<is_identifier>())
+		    return std::unexpected { "Expected Polaris command after '<'" };
+
+		result.command = read_while<is_identifier>();
+		while (expect_next<is_identifier>()) {
+		    const auto	param_name = read_while<is_identifier>();
+		    if (!expect_next<is_equals>())
+			return std::unexpected{ "Expected '=' after named parameter" };
+		    if (!expect_next<is_quote>())
+			return std::unexpected { "Expected String after named parameter" };
+		    const auto param_value = read_while<is_string>();
+
+		    result.parameters.emplace_back(param_name, ParsedParameter::Type::Identifier);
+		    result.parameters.emplace_back(param_value, ParsedParameter::Type::String);
+		}
+
+		if (pos >= current_line.cend() || *pos != '>')
+		    return std::unexpected{ "Expected '>' after command" };
+
+		return true;
+	    }
+
 	public:
-	    auto parse_line(const std::string_view& line)
+	    /**
+	     *
+	     */
+	    auto parse_line(const std::string& line)
 		-> std::expected<ParsedLine, std::string> {
 
-		bool		is_comment = false;
+		ParsedLine		result;
 
 		current_line = line;
 		line_nr++;
 		pos = current_line.cbegin();
 
-		while (!is_comment && pos != current_line.cend()) {
+		while (pos < current_line.cend()) {
 		    const char c = *pos;
 
 		    switch (c) {
 			case '#':
 			case '!':
-			    is_comment = true;
-			    break;
+			    return result;
 
 			case '"':
-			    break;
+			    result.parameters.emplace_back(
+				    read_while<is_string>(),
+				    ParsedParameter::Type::String);
+			    if (pos == current_line.end())
+				return std::unexpected{ "Missing '\"'" };
+			    continue;
 
 			case '<':
-			    break;
+			    if (const auto e = get_command(result);
+				not e) return std::unexpected{ e.error() };
+			    continue;
 
 			default:
 			    if (is_identifier(c))
-				read_while<is_identifier>();
-			    else if (is_number(c))
-				read_while<is_number>();
-			    else
-				return std::unexpected{ "Unknown Command" };
-		    }
-		}
-	    }
+				result.parameters.emplace_back(
+				    read_while<is_identifier>(), ParsedParameter::Type::Identifier);
 
+			    else if (is_number(c))
+				result.parameters.emplace_back(
+				    read_while<is_number>(), ParsedParameter::Type::Number);
+
+			    else
+				return std::unexpected{
+				    comp_error( "Unknown Token '", c, "'" ) };
+		    }
+
+		    ++pos;
+		}
+
+		return result;
+	    }
     };
 }
